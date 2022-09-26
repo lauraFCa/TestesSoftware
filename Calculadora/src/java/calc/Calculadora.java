4ab2084cb6e207d2c925a353b5771fa8c7148458
Laura C <laurafariacastro@outlook.com>
2022-09-25 21:06:27 -0300

    finish program 1

--- Calculadora/src/java/calc/Calculadora.java
+++ Calculadora/src/java/calc/Calculadora.java
@@ -19,17 +19,6 @@ public class Calculadora {
     }
     
     /**
-     * Web service operation: Subtracting of two numbers
-     * @param num1
-     * @param num2
-     * @return subtracting
-     */
-    @WebMethod(operationName = "Subtrair")
-    public int Subtrair(@WebParam(name = "num1") int num1, @WebParam(name = "num2") int num2) {
-        return num1-num2;
-    }
-    
-    /**
      * Web service operation: Multiplication of two numbers
      * @param num1
      * @param num2
